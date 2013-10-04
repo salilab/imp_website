0fc17b11b65b52a3b7c6ae2acf3dca3c306717c8
Ben Webb <ben@salilab.org>
2013-10-04 14:06:37 -0700

    Use relative links.

--- contact.php
+++ contact.php
@@ -6,7 +6,7 @@
 <div id="contact">
 
 <p>We provide several <a
-href="http://www.integrativemodeling.org/2.0.1/doc/html/maillists.html">
+href="2.0.1/doc/html/maillists.html">
 mailing lists</a> for discussion on using and developing IMP.</p>
 
 <p>To report problems with this website, you can also contact the
