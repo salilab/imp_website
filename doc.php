058b8a02e3feb3b5adde87e942365adada6c3121
Ben Webb <ben@salilab.org>
2014-04-10 11:43:42 -0700

    Link to old versions of the docs.

--- doc.php
+++ doc.php
@@ -21,17 +21,30 @@ tutorial</a></li>
   </ul>
 </li>
 
-<li>IMP 1.0 stable release
-  <ul>
-  <li><a href="1.0/tutorial/">Introductory tutorial</a></li>
-  <li><a href="1.0/doc/html/">Manual</a></li>
-  </ul>
-</li>
-
 </ul>
 
 <p>See also <a href="talks.html">the talks page</a> for various talks and other
 presentations about IMP.</p>
+
+<a name="old"></a>
+<h2>Old releases</h2>
+
+<ul>
+<li>2.1.1 <a href="2.1.1/doc/html">manual</a>;
+<a href="2.1.1/doc/tutorial/">tutorial</a></li>
+
+<li>2.1.0 <a href="2.1.0/doc/html">manual</a>;
+<a href="2.1.0/doc/tutorial/">tutorial</a></li>
+
+<li>2.0.1 <a href="2.0.1/doc/html">manual</a>;
+<a href="2.0.1/doc/tutorial/">tutorial</a></li>
+
+<li>2.0.0 <a href="2.0.0/doc/html">manual</a></li>
+
+<li>1.0 <a href="1.0/doc/html">manual</a>;
+<a href="1.0/tutorial/">tutorial</a></li>
+
+</ul>
 
 </div>
 
