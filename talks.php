5430392ae143c245f512172c30f93dde15f367f5
Ben Webb <ben@salilab.org>
2016-12-16 09:24:46 -0800

    Link to slides for the first day of our workshop.

--- talks.php
+++ talks.php
@@ -7,6 +7,15 @@
 and its applications.</p>
 
 <ul>
+<li>Slides from our December 2016
+<a href="https://www-s.ks.uiuc.edu/Training/Workshop/SanFrancisco2016/index.html">joint workshop with TCBG (VMD, NAMD, etc.)</a>
+  <ul>
+  <li><a href="talks/dec_2016_workshop/2016-12-14 Schulten Sali Workshop UCSF 90min.pdf">Introduction to Integrative Modeling and IMP</a></li>
+  <li><a href="talks/dec_2016_workshop/IMP software introduction and tutorial.pdf">IMP software introduction and tutorial</a></li>
+  <li><a href="talks/dec_2016_workshop/Modeling of the Nup84 complex.pdf">Modeling of the Nup84 complex</a></li>
+  </ul>
+</li>
+
 <li><a href="talks/IMP_tutorial_ACA_2016.key">Tutorial on using IMP and SAXS</a></li>
 <li><a href="talks/lawberk.pptx">Talk on AllosMod</a></li>
 <li><a href="talks/2014-04-05 Keystone 30min.key.zip">2014-04-05 Keystone 30min.key.zip</a></li>
