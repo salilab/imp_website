93d6032a69776d78d3eee966c589007215ee6eff
Ben Webb <ben@salilab.org>
2017-04-19 10:59:39 -0700

    Update to latest Fedora.

--- download.php
+++ download.php
@@ -62,7 +62,7 @@ version is tested every night; please check out the
 <a href="nightly/results/">nightly builds results
 page</a> to see if the code is currently stable enough for your purposes.</p>
 
-<p>Binary installers for Windows, Linux (Fedora 23, RHEL 5/6/7,
+<p>Binary installers for Windows, Linux (Fedora 25, RHEL 5/6/7,
 Ubuntu 12.04 LTS/14.04 LTS/16.04 LTS), and Mac OS X 10.6 or later
 <a href="nightly/download/">are also available</a>
 for the most recent nightly build that compiled successfully.</p>
