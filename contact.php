d88b91762e59a56cfccf55996324cf55c1d65b7c
Ben Webb <ben@salilab.org>
2022-09-13 16:53:57 -0700

    Link to new Mailman 3 mailing lists

--- contact.php
+++ contact.php
@@ -9,40 +9,36 @@
 <p>We provide several mailing lists for discussion on using and
 developing IMP.</p>
 
-<p>The <a href="//salilab.org/mailman/listinfo/imp-users">imp-users</a>
+<p>The <a href="//salilab.org/mm/postorius/lists/imp-users.salilab.org/">imp-users</a>
 mailing list is provided for <i>users</i> of IMP. It is for discussion of and
 help with the use of IMP. Both users and developers are encouraged to subscribe
 to the list. Prior postings are also
-<a href="//integrativemodeling.org/archives/imp-users/">archived</a>
-and <a href="search-imp-users.html">can be searched</a>.</p>
+<a href="//salilab.org/mm/hyperkitty/list/imp-users@salilab.org/latest">archived</a>
+and can be searched.</p>
 
-<p>The <a href="//salilab.org/mailman/listinfo/imp-dev">imp-dev</a>
+<p>The <a href="//salilab.org/mm/postorius/lists/imp-dev.salilab.org/">imp-dev</a>
 mailing list is provided for <i>developers</i> of IMP. It is for technical
 discussion of the development of new features in IMP, such as the design of
 new C++ or Python classes. Developers are encouraged to subscribe, but regular
 users are encouraged to use the
-<a href="//salilab.org/mailman/listinfo/imp-users">imp-users</a> list
+<a href="//salilab.org/mm/postorius/lists/imp-users.salilab.org/">imp-users</a> list
 instead. Prior postings are also
-<a href="//integrativemodeling.org/archives/imp-dev/">archived</a>
-and <a href="search-imp-dev.html">can be searched</a>.</p>
+<a href="//salilab.org/mm/hyperkitty/list/imp-dev@salilab.org/latest">archived</a>
+and can be searched.</p>
 
-<p>The <a href="//salilab.org/mailman/listinfo/imp-build">imp-build</a>
+<p>The <a href="//salilab.org/mm/postorius/lists/imp-build.salilab.org/">imp-build</a>
 list sends out an email every morning with the results of the nightly builds
 of IMP. All developers are encouraged to subscribe to this list, so they can
-be notified if they break the build. Developers that work only on a subset
-of modules can choose to be notified only if one of those modules fails
-(go to the <a href="//salilab.org/mailman/listinfo/imp-build">imp-build</a>
-page, select 'Unsubscribe or edit options', log in, then select one or more
-modules from the topic filters.)</p>
+be notified if they break the build.</p>
 
-<p>The <a href="//salilab.org/mailman/listinfo/imp-commits">imp-commits</a>
+<p>The <a href="//salilab.org/mm/postorius/lists/imp-commits.salilab.org/">imp-commits</a>
 list sends out an email every time code is changed in the IMP git repository.
 It can be a high traffic list, and is only recommended for developers that
 want to keep an eye on new developments.
 (There are similar
-<a href="//salilab.org/mailman/listinfo/rmf-commits">RMF-commits</a>
+<a href="//salilab.org/mm/postorius/lists/rmf-commits.salilab.org/">RMF-commits</a>
 and
-<a href="//salilab.org/mailman/listinfo/pmi-commits">PMI-commits</a>
+<a href="////salilab.org/mm/postorius/lists/pmi-commits.salilab.org/">PMI-commits</a>
 lists for <a href="https://integrativemodeling.org/rmf/nightly/doc/">RMF</a>
 and <a href="https://integrativemodeling.org/nightly/doc/ref/namespaceIMP_1_1pmi.html">PMI</a>
 respectively.)</p>
