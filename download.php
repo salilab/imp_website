16510768b401374ad51d0cb721add16d2f547143
Ben Webb <ben@salilab.org>
2024-07-14 23:06:52 -0700

    Drop Mac installer and EOL'd RHEL 7

--- download.php
+++ download.php
@@ -62,8 +62,8 @@ version is tested every night; please check out the
 <a href="nightly/results/">nightly builds results
 page</a> to see if the code is currently stable enough for your purposes.</p>
 
-<p>Binary installers for Windows, Linux (Fedora, RHEL 7/8/9,
-Ubuntu 20.04 LTS/22.04 LTS/24.04 LTS), and Mac OS X 10.10 or later
+<p>Binary installers for Windows, and for Linux (Fedora, RHEL 8/9,
+Ubuntu 20.04 LTS/22.04 LTS/24.04 LTS)
 <a href="nightly/download/">are also available</a>
 for the most recent nightly build that compiled successfully.
 <a href="download-anaconda.html">Anaconda packages</a> for 64-bit Linux are
