f07bb56afbff1cf658683e44317b88e095d250aa
Ben Webb <benmwebb@gmail.com>
2025-10-02 22:39:58 -0700

    Add missing file

---
+++ imp-users.php
@@ -0,0 +1,38 @@
+<?php
+   include("IMP.inc");
+   print_page_header("IMP-users");
+?>
+
+<div id="contact">
+
+<p>The IMP-users mailing list is provided for users of IMP. It is for
+discussion of and help with the use of IMP, as well as for discussion of
+the development of new features in IMP, such as the design of new C++ or
+Python classes.</p>
+
+<p>Prior postings to the list
+<a href="//integrativemodeling.org/archives/imp-users/">are archived</a> and
+<a href="search-imp-users.html">can be searched</a>.</p>
+
+<p>(Previously there was also an IMP-dev list purely for developers, but
+this discussion now happens on the IMP-users list. However the prior IMP-dev
+posts <a href="//integrativemodeling.org/archives/imp-dev/">are archived</a>
+and <a href="search-imp-dev.html">can be searched</a>.)</p>
+
+<p>To subscribe to the IMP-users list, send an email to
+<span class="e-mail">listserv at listsrv.ucsf.edu | listserv@listsrv.ucsf.edu</span>.
+Leave the Subject field blank and remove your signature from the email.
+In the body of the message, enter just:
+<tt>SUBSCRIBE imp-users firstname lastname</tt> where <tt>firstname</tt>
+is your first name and <tt>lastname</tt> your last name.</p>
+
+<p>Other listserv commands can be sent in the same way to the same email
+address. For example, <tt>SIGNOFF imp-users</tt> will cancel your
+subscription to the list.
+See the <a href="https://www.lsoft.com/manuals/16.0/htmlhelp/list%20subscribers/LSCommands.html">LISTSERV manual</a> for more information.</p>
+
+</div>
+
+<?php
+   print_page_footer();
+?>
